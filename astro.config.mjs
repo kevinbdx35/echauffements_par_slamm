ad6b78b7b2d7e527915ad9cf46ff7006d72fbc03
Kevin <k.bardoux@orange.fr>
2026-02-25 23:22:02 +0100

    fix: add trailing slash to base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://kevinbdx35.github.io',
-  base: '/echauffements_par_slamm',
+  base: '/echauffements_par_slamm/',
   integrations: [tailwind()],
 });
