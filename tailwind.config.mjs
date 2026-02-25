19c46c2c2c258ff7b1f939b11eb22195100defbf
Kevin <k.bardoux@orange.fr>
2026-02-25 22:55:23 +0100

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          dark: '#0f172a',
+          accent: '#ef4444',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
